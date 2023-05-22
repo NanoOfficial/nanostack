f499547dccf15b96959c69d62baaf2692e364d95
krishpranav <krisna.pranav@gmail.com>
2023-05-22 19:55:40 +0530

    supportedlang: lib[en, cs]

--- src/supportedlang/src/lib.rs
+++ src/supportedlang/src/lib.rs
@@ -4,3 +4,33 @@
  * @copyright: COPYRIGHT (2023) MIT LICENSE Krisna Pranav
 */
 
+use std::{borrow::Cow, fmt};
+
+#[derive(Clone)]
+pub enum Lang {
+    English,
+    French,
+    Czech,
+    Norwegian,
+    Spanish,
+    Swedish,
+}
+
+impl Lang {
+    pub fn as_str(&self) -> &str {
+        match self {
+            Self::English => "en",
+            Self::Czech => "cz",
+            Self::French => "fr",
+            Self::Norwegian => "no",
+            Self::Spanish => "es",
+            Self::Swedish => "sv",
+        }
+    }
+}
+
+impl fmt::Display for Lang {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.as_str())
+    }
+}
