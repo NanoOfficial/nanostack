2e10d3c8536c4d2f1f675f377a5da46479692085
krishpranav <krisna.pranav@gmail.com>
2023-05-23 12:45:21 +0530

    stackcore: session_id

---
+++ src/stackcore/src/session_id.rs
@@ -0,0 +1,30 @@
+/**
+ * @filename: session_id.rs
+ * @author: Krisna Pranav
+ * @copyright: COPYRIGHT (2023) MIT LICENSE Krisna Pranav
+*/
+use crate::*;
+use std::{fmt, str::FromStr};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct SessionId(Ulid);
+
+impl SessionId {
+    pub fn new() -> Self {
+        SessionId(Ulid::generate())
+    }
+}
+
+impl fmt::Display for SessionId {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl FromStr for SessionId {
+    type Err = DecodingError;
+
+    fn from_str(session_id: &str) -> Result<Self, Self::Err> {
+        Ok(SessionId(session_id.parse()?))
+    }
+}
