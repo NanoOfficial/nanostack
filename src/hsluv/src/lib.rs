2fcb40d7613ecc26eca3773189cb85243949fcc9
krishpranav <krisna.pranav@gmail.com>
2023-05-31 18:36:57 +0530

    hsluv: lib

--- src/hsluv/src/lib.rs
+++ src/hsluv/src/lib.rs
@@ -12,6 +12,30 @@ pub struct HSLuv {
     a: f64,
 }
 
-// impl fmt::Display for HSLuv {}
+impl fmt::Display for HSLuv {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let (r, g, b) = self.to_rgb();
+        let (r, g, b, a) = (r * 100., g * 100., b * 100., self.a);
+        write!(f, "rgba({r}% {g}% {b}% / {a}%)")
+    }
+}
 
-impl HSLuv {}
+impl HSLuv {
+    pub fn hsl(h: impl Into<f64>, s: impl Into<f64>, l: impl Into<f64>) -> Self {
+        Self::hsla(h, s, l, 100)
+    }
+
+    pub fn hsla(
+        h: impl Into<f64>,
+        s: impl Into<f64>,
+        l: impl Into<f64>,
+        a: impl Into<f64>,
+    ) -> Self {
+        Self {
+            h: h.into().clamp(0., 360.),
+            s: s.into().clamp(0., 100.),
+            l: l.into().clamp(0., 100.),
+            a: a.into().clamp(0., 100.),
+        }
+    }
+}
